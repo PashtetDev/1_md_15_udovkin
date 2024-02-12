3cd7a927935270c88eae3b8da1d47c801b061ad8
skai68r <liza.01042002.com@gmail.com>
2024-02-12 14:27:11 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 #task1
-print('First commit!')
+#print('First commit!')
+
+import requests
+from bs4 import BeautifulSoup
+
+
+url = "https://www.gismeteo.ru/weather-sankt-peterburg-4079/"
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
+
+city = soup.find('h1').text
+
+print(city)
